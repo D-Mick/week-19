044e455dff737b5a81616cdc683135ae91562931
unknown <durotimijohnson@gmail.com>
2021-05-30 12:53:46 +0400

    Added few pics

---
+++ product.js
@@ -0,0 +1,52 @@
+let products = [
+    {
+    id: 111,
+    title: "Cat Food, 25lb bag",
+    description: "A 25 pound bag of <em> irresistible </em>," +
+        "organic goodness for your cat.",
+    price: 2000,
+    image: 'image/logo_ 1.png',
+    availableInventory: 10,
+    rating: 3
+},
+{
+    id: 112,
+    title: "Dog Food, 25lb bag",
+    description: "A 25 pound bag of <em> irresistible </em>," +
+        "organic goodness for your cat.",
+    price: 2000,
+    image: 'image/logo_ 1.png',
+    availableInventory: 10,
+    rating: 5
+},
+{
+    id: 113,
+    title: "Fouad Food, 25lb bag",
+    description: "A 25 pound bag of <em> irresistible </em>," +
+        "organic goodness for your cat.",
+    price: 2000,
+    image: 'image/logo_ 1.png',
+    availableInventory: 10,
+    rating: 4
+},
+{
+    id: 114,
+    title: "Blessed Food, 25lb bag",
+    description: "A 25 pound bag of <em> irresistible </em>," +
+        "organic goodness for your cat.",
+    price: 2000,
+    image: 'image/logo_ 1.png',
+    availableInventory: 10,
+    rating: 2
+},
+{
+    id: 115,
+    title: "Emma Food, 25lb bag",
+    description: "A 25 pound bag of <em> irresistible </em>," +
+        "organic goodness for your cat.",
+    price: 2000,
+    image: 'image/logo_ 1.png',
+    availableInventory: 10,
+    rating: 1
+},
+]
