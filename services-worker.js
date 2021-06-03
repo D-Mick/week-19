10400d46d0ae071715b661218612f1d8b4df7337
unknown <durotimijohnson@gmail.com>
2021-06-03 13:35:50 +0400

    Updated

--- services-worker.js
+++ services-worker.js
@@ -6,13 +6,41 @@ var cacheFiles = [
     'image/logo_ 1.png',
     'image/weblogo.png'
 ];
-
+//Installing the cache files
+//self means the current brower
 self.addEventListener('install', (e) => {
     console.log('[Service Worker] Install');
     e.waitUntil(
         caches.open(cacheName).then((cache) => {
             console.log('[Service Worker] Caching all the files');
             return cache.addAll(cacheFiles);
+        })
+    );
+});
+
+//respond with checks if the files exists
+// self.addEventListener('fetch'
+// , function (e) {
+// e.respondWith(
+// caches.match(e.request).then(function (r) {
+// console.log('[Service Worker] Fetching resource: '
+// + e.request.url);
+// return r
+// })
+// );
+// });
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith(
+        caches.match(e.request).then(function (r) {
+            // Download the file if it is not in the cache,
+            return r || fetch(e.request).then(function (response) {
+                // add the new file to cache
+                return caches.open(cacheName).then(function (cache) {
+                    cache.put(e.request, response.clone());
+                    return response;
+                });
+            });
         })
     );
 });
